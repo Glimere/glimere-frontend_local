f351ed048bf9cd07102cbab4897e5dbcaab88d46
Joebim <josey359@gmail.com>
2025-01-30 13:22:09 +0100

    [FIX] type issues

--- src/components/landing/Bubble.tsx
+++ src/components/landing/Bubble.tsx
@@ -1,9 +1,14 @@
-const Bubble = (props) => {
+interface BubbleProps {
+  classes: string;
+  img: string;
+}
+
+const Bubble = (props: BubbleProps) => {
   return (
-  <div className={`absolute rounded-full duration-500 bg-cover bg-center bg-no-repeat border-solid border-[7px] border-[#272727] ${props.classes}`}
-        style={{ backgroundImage: `url(${props.img})` }}
-  >
-  </div>
+    <div className={`absolute rounded-full duration-500 bg-cover bg-center bg-no-repeat border-solid border-[7px] border-[#272727] ${props.classes}`}
+         style={{ backgroundImage: `url(${props.img})` }}
+    >
+    </div>
   );
 };
 export default Bubble;
